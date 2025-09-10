bc4bd687498e7a5e0eb966affd8d30276d2adad5
ys kim <tbvjdkfdir@gmail.com>
2025-09-11 08:30:46 +0900

    클라이언트 Send 함수 및 Recv 콜백 기초 구현
    - 프로토콜 정의
    - Recv 패킷 파싱 및 함수 호출

---
+++ Assets/Network_Kys/02. Scripts/Protocol.cs
@@ -0,0 +1,17 @@
+public enum PROTOCOL : ushort
+{
+    ROOM_REQUEST = 0,  // 방 목록 받음
+    ROOM_CREATE,       // 방 목록 생성 결과
+    ROOM_JOIN,         // 방 참가
+    ROOM_EXIT,         // 방 퇴장
+    GAME_DO,           // 상대방 착수 결과
+    GAME_WIN,           // 상대방 승리 Alert
+    GAME_START,        // 게임 시작
+}
+
+public enum GameResultState : byte
+{
+    Win = 0,
+    Lose,
+    Draw
+}
