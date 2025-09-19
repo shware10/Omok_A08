9ab9a9f52694b25bda04b2d0ae858edaefefe9c1
shware <112537781+shware10@users.noreply.github.com>
2025-09-19 14:02:37 +0900

    멀티플레이 오목매니저

--- Assets/02.Scripts/MultiOmok/OmokManager_Multi.cs
+++ Assets/02.Scripts/MultiOmok/OmokManager_Multi.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class OmokManager_Multi : MonoBehaviour
 {
-    public static OmokManager Instance;
+    public static OmokManager_Multi Instance;
 
     [Header("Board")]
     [SerializeField] public int N = 15;
-
     public LayerMask cellMask;
+    private Board board;
 
-    Board board;
-    public StoneState turn = StoneState.Black;                                      // 처음 시작시 흑이 시작
+    [Header("Online")]
+    public bool isHost = true;
+    public StoneState curTurn = StoneState.Black;                                      // 처음 시작시 흑이 시작
+    public StoneState myStone = StoneState.Black;
+    public int turnNo = 0;
 
     public GameState curState;
 
@@ -32,8 +35,7 @@ public class OmokManager_Multi : MonoBehaviour
 
     void Awake()
     {
-        board = new Board(N);
-        SetState(GameState.None);
+        Instance = this;
     }
 
     void Start()
@@ -50,37 +52,64 @@ public class OmokManager_Multi : MonoBehaviour
         var FSListeners = FindObjectsByType<MonoBehaviour>(FindObjectsInactive.Include, FindObjectsSortMode.None)
                   .OfType<IForbiddenSelectListener>();
         foreach (var listener in FSListeners) OnForbbidenSeleted += listener.OnForbbidenSeleted;
+
+        NetworkManager networkManager = NetworkManager.Instance;
+        networkManager.room_create_act += (int isSuccess) => { if(isSuccess == 1) isHost = true; };
+        networkManager.room_join_act += (int isSuccess) => { if (isSuccess == 1) isHost = false; };
+        networkManager.move_req_act += OnMoveReq;
+        networkManager.move_com_act += OnMoveCommit;
+        networkManager.game_result_act += OnGameResult;
+        networkManager.game_start_act += OnGameStart;
     }
 
     void Update()
     {
-        if(curState == GameState.None)
+        if (curState != GameState.None) return;
+        if (myStone != curTurn) return;
+
+        if (Input.GetMouseButtonDown(0))
         {
-            if (Input.GetMouseButtonDown(0)) TryPlace();
+            if (isHost) TryPlace_Host();
+            else TryPlace_Guest();
         }
+    }
+
+
+    void TryPlace_Host()
+    {
+        if (!RayToCell(out int x, out int y)) return;
+        TryHostCommit(x, y);
+    }
+
+    void TryPlace_Guest()
+    {
+        if (!RayToCell(out int x, out int y)) return;
+        // 호스트로 요청
+        NetworkManager.Instance.Send_Move_Request((byte)x, (byte)y);
     }
 
     /// <summary>
     /// 해당 칸이 빈칸인지 먼저 확인하기위한 기능
     /// </summary>
     /// <param name="screenPos"></param>
-    void TryPlace()
+    bool RayToCell(out int x, out int y)
     {
-        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(r, out var hit, 100f, cellMask))
-        {
-            Cell cell = hit.collider.GetComponent<Cell>(); //cell이 없는 
+        x = y = -1;
 
-            if (cell != null)
-            {
-                OnStone(cell.X, cell.Y);
-            }
-        }
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(r, out var hit, 100f, cellMask)) return false;
+
+        Cell cell = hit.collider.GetComponent<Cell>(); 
+        if (cell != null) return false;
+
+        x = cell.X; y = cell.Y;
+
+        return true;
     }
 
-    void OnStone(int x, int y)
+    void TryHostCommit(int x, int y)
     {
-        if (turn == StoneState.Black)
+        if (curTurn == StoneState.Black)
         {
             if (board.IsForbiddenBlackRock(x, y))   //렌쥬룰 체크
             {
@@ -90,34 +119,104 @@ public class OmokManager_Multi : MonoBehaviour
             }
         }
 
-        board.Place(x, y, turn);                    // 보드에 수를 놓으면
+        board.Place(x, y, curTurn);                    // 보드에 수를 놓으면
         Debug.Log("돌 놓기 완료");
-        OnBoardChanged?.Invoke(x, y, turn);         // 보드 뷰 업데이트
-        board.ShowBoard();
+        OnBoardChanged?.Invoke(x, y, curTurn);         // 보드 뷰 업데이트
 
-        if (board.CheckWin(x, y, turn))
+        //브로드 캐스트
+        ushort nextTurnNo = (ushort)(turnNo + 1);
+        byte colorByte = (byte)(curTurn == StoneState.Black ? 0 : 1);
+        NetworkManager.Instance.Send_Move_Commit(nextTurnNo, (byte)x, (byte)y, colorByte);
+        turnNo = nextTurnNo;
+
+        if (board.CheckWin(x, y, curTurn))
         {
-            if (turn == StoneState.White)           //흰색 승리
-            {
-                SetState(GameState.WhiteWin);
-                Debug.Log("백 승리");
-                return;
-            }
-            else if (turn == StoneState.Black)      //검은색 승리
-            {
-                SetState(GameState.BlackWin);
-                Debug.Log("흑 승리");
-                return;
-            }
+            byte result = (curTurn == StoneState.Black) ? (byte)1 : (byte)0;
+            SetState(curTurn == StoneState.Black ? GameState.BlackWin : GameState.WhiteWin);
+            //게스트로 브로드 캐스트
+            NetworkManager.Instance.Send_Game_Result(result);
+            return;
         }
 
         if (board.IsFull())
         {
             SetState(GameState.Draw);               //승리 없이 돌이 다 찼으면 무승부
+            //게스트로 브로드 캐스트
+            NetworkManager.Instance.Send_Game_Result((byte)3);
             Debug.Log("무승부");
             return;
         }
 
-        turn = turn == StoneState.Black ? StoneState.White : StoneState.Black;
+        curTurn = curTurn == StoneState.Black ? StoneState.White : StoneState.Black;
+    }
+
+    void OnGameResult(byte result)
+    {
+        if (isHost) return;
+
+        if (result == 1)        SetState(GameState.BlackWin);
+        else if (result == 2)   SetState(GameState.WhiteWin);
+        else if (result == 3)   SetState(GameState.Draw);
+    }
+
+    //호스트가 게스트 요청 수신
+    void OnMoveReq(byte x, byte y)
+    {
+        if (!isHost) return;
+
+        TryHostCommit(x, y);
+    }
+
+    void OnMoveCommit(ushort recvTurnNo, byte bx, byte by, byte bcolor)
+    {
+        if (isHost) return;
+
+        if (recvTurnNo <= turnNo) return; //중복/역순 보호
+        turnNo = recvTurnNo;
+
+        int x = bx, y = by;
+        StoneState color = (bcolor == 0) ? StoneState.Black : StoneState.White;
+
+        board.Place(x, y, bcolor == 0 ? StoneState.Black : StoneState.White);
+        OnBoardChanged?.Invoke(x, y, bcolor == 0 ? StoneState.Black : StoneState.White);
+
+        curTurn = (bcolor == 0) ? StoneState.White : StoneState.Black;
+    }
+
+    /// <summary>
+    /// 서버에 송신 호스트에서만 실행
+    /// </summary>
+    public void HostStartGame() 
+    {
+        if (!isHost) return;
+
+        bool randBool = (Random.Range(0, 2) == 0);
+        myStone = randBool ? StoneState.Black : StoneState.White;
+
+        curTurn = StoneState.Black;
+        turnNo = 0;
+        board = new Board(N);
+        SetState(GameState.None);
+
+        byte blackIsHost = randBool ? (byte)1 : (byte)0;
+
+        NetworkManager.Instance.Send_Game_Start(blackIsHost);
+    }
+    /// <summary>
+    /// 수신될 서버에서 브로드 캐스트
+    /// </summary>
+    /// <param name="blackIsHost"></param>
+    void OnGameStart(byte blackIsHost)
+    {
+        if (isHost) return;
+
+        bool hostIsBlack = (blackIsHost == 1);
+
+        myStone = hostIsBlack ? StoneState.White : StoneState.Black; //호스트가 블랙이면 나는 화이트
+
+        curTurn = StoneState.Black;
+        turnNo = 0;
+        board = new Board(N);
+        SetState(GameState.None);
     }
 }
